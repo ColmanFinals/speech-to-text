0411661b907fd8d962a42ef868b6260a5f94c1be
Bar Agmon <48044742+BarAgmon@users.noreply.github.com>
2024-06-26 14:43:37 +0300

    Update gemini_helper.py

--- gemini_helper.py
+++ gemini_helper.py
@@ -4,55 +4,50 @@ import os
 
 GOOGLE_API_KEY= os.environ["GOOGLE_API_KEY"]
 SYSTEM_INSTRUCTIONS= f"""
-Given a sentence, return a single action word (for example: "start," "stop," "repeat") that the sentence implies.
-The output is the action only. without any additional information 
-The output should be one of the following actions:
-1. {SUPPORTED_COMMANDS}
-2. false for any other case where the output doesn't describe one of the words above.
-Output can be only one of the following actions or false in any other case. This also means that output will always be in English.
-Be aware of cases where the word you get can be close during speaking to one of the actions, and was translated wrongly from speech to text- therefore will be recognized as an action.
-*Examples:*
+Given a user's input, classify their intent, to one of the following commands: {SUPPORTED_COMMANDS} if possible else false. 
+Answer a single command or false if the command is not comprehendable.
 
-1. *Sentence:* Begin the process.
-   *Result:* start
+Text: Begin the process.
+Answer: start
 
-2. *Sentence:* Cease all operations.
-   *Result:* stop
+Text: Cease all operations.
+Answer: stop
 
-3. *Sentence:* Do it once more.
-   *Result:* repeat
+Text: Do it once more.
+Answer: repeat
 
-4. *Sentence:* Let's go again.
-   *Result:* repeat
+Text: Let's go again.
+Answer: repeat
 
-5. *Sentence:* Initiate the program.
-   *Result:* start
+Text: Initiate the program.
+Answer: start
 
-6. *Sentence:* Put everything on hold.
-   *Result:* stop
+Text: Put everything on hold.
+Answer: stop
 
-7. *Sentence:* Hi
-   *Result:* false
+Text: Hi
+Answer: false
 
-8. *Sentence:* mkelvnldr
-   *Result:* false
-   
-9. *Sentence:* stump // can be a mistake during translation from speech to text, and sounds similar to start.
-   *Result:* start 
+Text: mkelvnldr
+Answer: false
 
-10. *Sentence:* בצלגללםכם
-    *Result:* false
+Text: stump // can be a mistake during translation from speech to text, and sounds similar to start.
+Answer: start 
+
+Text: בצלגללםכם
+Answer: false
     
-11. *Sentence:* Lo siento
-    *Result:* false
-
+Text: Lo siento
+Answer: false 
 """
+
 
 GEMINI_MODEL="gemini-1.5-flash-latest"
 
 def init_gemini_custom_prompt():
     genai.configure(api_key=GOOGLE_API_KEY)
     model = genai.GenerativeModel(
+        generation_config=genai.types.GenerationConfig(topk=1, temprature=0, max_output_tokens=10),
         model_name=GEMINI_MODEL,
         system_instruction=[SYSTEM_INSTRUCTIONS],
     )
